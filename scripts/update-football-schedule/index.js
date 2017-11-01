5a6d548f156c17a1b546dce05e74559f9f7f3e38
MatthieuLemoine <ml.94230@gmail.com>
2017-11-01 16:33:27 +0100

    :sparkles: Add logger && retry on failure

--- scripts/update-football-schedule/index.js
+++ scripts/update-football-schedule/index.js
@@ -1,6 +1,10 @@
 // Will be run every day by pm2 using process.json
-const chalk = require('chalk');
+
+// eslint-disable-next-line no-global-assign
+require = require('@std/esm')(module, { cjs: true, esm: 'js' });
+
 const { request } = require('graphql-request');
+const logger = require('../../server/logger').default;
 
 const PORT = process.env.PORT || 3888;
 const URL = `http://localhost:${PORT}/graphql`;
@@ -19,13 +23,20 @@ const query = `{
   }
 }`;
 
-update()
-  .then(() => console.log(chalk.green('Football schedule updated')))
-  .catch(console.error);
+logger.info('Script started');
+run();
 
-function update() {
-  return request(URL, query).catch(() => {
-    console.log(chalk.yellow('Error while scraping football schedule, retrying...'));
-    return update();
-  });
+async function run(retries = 0) {
+  try {
+    await request(URL, query);
+    logger.info('Football schedule updated !');
+  } catch (e) {
+    if (retries >= 10) {
+      logger.error(`Request has failed ${retries} times. Giving up.`);
+      return;
+    }
+    logger.error(`Request has failed. Retry number ${retries + 1}.`);
+    // Retry in 10 minuues
+    setTimeout(() => run(retries + 1), 600000);
+  }
 }
--- scripts/update-showtimes/index.js
+++ scripts/update-showtimes/index.js
@@ -1,5 +1,10 @@
 // Will be run every day by pm2 using process.json
+
+// eslint-disable-next-line no-global-assign
+require = require('@std/esm')(module, { cjs: true, esm: 'js' });
+
 const { request } = require('graphql-request');
+const logger = require('../../server/logger').default;
 
 const PORT = process.env.PORT || 3888;
 const URL = `http://localhost:${PORT}/graphql`;
@@ -15,6 +20,20 @@ const query = `{
   }
 }`;
 
-request(URL, query)
-  .then(() => process.stdout.write('Showtimes updated\n'))
-  .catch(console.error);
+logger.info('Script started');
+run();
+
+async function run(retries = 0) {
+  try {
+    await request(URL, query);
+    logger.info('Showtimes updated !');
+  } catch (e) {
+    if (retries >= 10) {
+      logger.error(`Request has failed ${retries} times. Giving up.`);
+      return;
+    }
+    logger.error(`Request has failed. Retry number ${retries + 1}.`);
+    // Retry in 10 minuues
+    setTimeout(() => run(retries + 1), 600000);
+  }
+}
